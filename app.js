c66858fb37c8d8edbb5f6a975990c9751d67eb56
MD Huq <samiulg3@gmail.com>
2016-01-03 14:43:54 -0800

    angular setup

--- app.js
+++ app.js
@@ -23,6 +23,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+//add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://clicktime.herokuapp.com/api/1.0');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,22 +1,150 @@
 var app = angular.module("clicktime", ['ngResource', 'ui.router']);
 
+app.run(($rootScope) => {
+  $rootScope.$on("$stateChangeError", console.log.bind(console));
+});
+
+app.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+}]);
+
+
 app.constant("app", {
   "BASE_URL": "https://clicktime.herokuapp.com/api/1.0"
 });
 
-app.controller("home", function($scope, User) {
+
+app.controller("home", function($scope, User, $http, $resource, app, TasksFactory, JobsFactory, ClientsFactory) {
+
+  $("#name").html(User.user.UserName);
+  $scope.user = User.user;
+
+
+  TasksFactory.getTasks().then(function(data) {
+    $scope.tasks = data;
+  });
+  JobsFactory.getJobs().then(function(data) {
+    $scope.jobs = data;
+  });
+  ClientsFactory.getClients().then(function(data){
+    $scope.clients = data;
+  });
+
+  
 });
 
+app.factory("TasksFactory", function($http, $q, app, User) {
+  var o = {
+    tasks: {}
+  }
 
-app.factory("User", function($http, $q, $timeout ) {
+  o.getTasks = function() {
+    var deferred = $q.defer();
+
+    $http.jsonp(app.BASE_URL + "/Companies/" + User.user.CompanyID + "/Users/" + User.user.UserID + '/Tasks/?callback=JSON_CALLBACK')
+      .success(function(data) {
+        o.tasks = data;
+        deferred.resolve(data);
+      }).error(function(err) {
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  }
+
+  return o;
+});
+
+app.factory("ClientsFactory", function($http, $q, app, User) {
+  var o = {
+    clients: {}
+  }
+
+  o.getClients = function() {
+    var deferred = $q.defer();
+
+    $http.jsonp(app.BASE_URL + "/Companies/" + User.user.CompanyID + "/Users/" + User.user.UserID + '/Clients/?callback=JSON_CALLBACK')
+      .success(function(data) {
+        o.clients = data;
+        deferred.resolve(data);
+      }).error(function(err) {
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  }
+
+  return o;
+});
+
+app.factory("JobsFactory", function($http, $q, app, User) {
+  var o = {
+    jobs: {}
+  }
+
+  o.getJobs = function() {
+    var deferred = $q.defer();
+
+    $http.jsonp(app.BASE_URL + "/Companies/" + User.user.CompanyID + "/Users/" + User.user.UserID + '/Jobs/?withChildIDs=true&callback=JSON_CALLBACK')
+      .success(function(data) {
+        o.jobs = data;
+        deferred.resolve(data);
+      }).error(function(err) {
+        debugger;
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  }
+
+  return o;
+})
+
+app.factory("User", function($http, $q, $timeout, app) {
   var o = {
     user: {}
+  }
+
+  o.getUser = function() {
+    var deferred = $q.defer();
+
+    //Promise not working with $http.jsonp(...), so resorting to $.ajax
+    $.ajax(app.BASE_URL + '/session', {
+        dataType:'jsonp',
+        success: function(response) { 
+          console.log(response);
+          o.user = response;
+          deferred.resolve(response);
+        }
+    });
+    return deferred.promise;
   }
 
   return o;
 });
 
 
-app.factory("UserResource", function($resource, app) {
-  return $resource(app.BASE_URL + "/Companies/:company_id/Users/:user_id")
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html',
+      controller: 'home',
+      resolve: {
+        getPromise: ['User', function(User) {
+          return User.getUser();
+        }]
+      }
+    })
+    .state('tasks', {
+      url: '/tasks',
+      controller: 'tasks',
+      templateUrl: 'tasks.html',
+      resolve: {
+        getPromise: ['TasksFactory', 'User', function(TasksFactory, User) {
+        }]
+      }
+    });
 });
