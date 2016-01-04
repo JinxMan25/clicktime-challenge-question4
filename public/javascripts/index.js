63bed2bc6c3303e15842e7204f53f5af2322cab1
MD Huq <samiulg3@gmail.com>
2016-01-04 15:23:32 -0800

    show tasks page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,30 +5,27 @@ app.run(function($rootScope){
   $rootScope.$on("$stateChangeError", console.log.bind(console));
 
   //show loading indicator when state starts to change
-  $rootScope.$on("$stateChangeStart", function() {
+  $rootScope.$on("$stateChangeStart", function(){
     $rootScope.loading = true;
   });
 
   //hide loading indicator when state loads
-  $rootScope.$on("$stateChangeSuccess", function( ){
+  $rootScope.$on("$stateChangeSuccess", function(){
     $rootScope.loading = false;
   });
 });
 
-app.config(['$httpProvider', function($httpProvider) {
+app.config(['$httpProvider', function($httpProvider){
   $httpProvider.defaults.useXDomain = true;
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-
 }]);
-
 
 //Constants
 app.constant("app", {
   "BASE_URL": "https://clicktime.herokuapp.com/api/1.0"
 });
 
-
-app.controller("home", function($rootScope, $scope, User, $http, $resource, app, TasksFactory, JobsFactory, ClientsFactory) {
+app.controller("home", function($rootScope, $scope, User, $http, $resource, app, TasksFactory, JobsFactory, ClientsFactory, local) {
 
   $rootScope.user = User.user;
 
@@ -46,7 +43,7 @@ app.controller("home", function($rootScope, $scope, User, $http, $resource, app,
 });
 
 //controller for /tasks route
-app.controller("tasks", function($rootScope, $scope, User, TasksFactory, $location) {
+app.controller("tasks", function($rootScope, $scope, User, TasksFactory, $location, local) {
   //Show all the tasks at first
   $scope.tasks = TasksFactory.tasks; 
 
@@ -57,35 +54,45 @@ app.controller("tasks", function($rootScope, $scope, User, TasksFactory, $locati
   var f = new Fuse(TasksFactory.tasks, options)
 
   //Show all tasks if input search bar is empty (when user backspaces until search bar is empty), otherwise it'll show no results
-  $scope.keyUp = function(e){
+  $scope.keyUp = function(e) {
     if (e == 8 && $scope.search == ""){
       $scope.tasks = TasksFactory.tasks; 
     }
     
     //If search result narrows down to one result, user has the option to hit the enter button on the keyboard to be directed to the particular tasks page
     if (e == 13 && $scope.tasks.length == 1) {
-      $location.url("/tasks/" + $scope.tasks[0].TaskID);
+      $scope.goToTask();
     }
+  }
+
+  $scope.goToTask = function(){
+    $location.url("/tasks/" + $scope.tasks[0].TaskID);
   }
 
   //Fuzzy search through the tasks for every key stroke on the search model
   $scope.$watch("search", function() {
     $scope.tasks = f.search($scope.search);
+    if ($scope.tasks.length == 1){
+      $scope.enter = true;
+    } else {
+      $scope.enter = false;
+    }
   });
 
 });
 
 //Controller for /tasks/:task_id route
 //Shows all the jobs and clients pertaning to task_id
-app.controller("task", function($scope, $stateParams, User, TasksFactory, ClientsFactory, JobsFactory){
+app.controller("task", function($scope, $stateParams, User, TasksFactory, ClientsFactory, JobsFactory, local){
   //This hasn't been tested but these are the dataset pertaning to a particular task. 
+
   $scope.jobs = JobsFactory.job;
   $scope.clients = ClientsFactory.client;
 });
 
 //Tasks factory that stores all tasks for the user and one more task for the /tasks/:task_id route
 //The additional task is actually unecessary but is present for testing purposes
-app.factory("TasksFactory", function($http, $q, app, User) {
+app.factory("TasksFactory", function($http, $q, app, User, local) {
   var o = {
     tasks: {},
     task: {}
@@ -96,9 +103,16 @@ app.factory("TasksFactory", function($http, $q, app, User) {
 
     $.ajax(app.BASE_URL + "/Companies/" + User.user.CompanyID + "/Users/" + User.user.UserID + '/Tasks', {
         dataType:'jsonp',
+        timeout: 100,
         success: function(response) { 
           o.tasks = response;
           deferred.resolve(response);
+        },
+        error: function(xhr, stat, error) {
+          local.resource("tasks").then(function(data){
+            o.tasks = data;
+            deferred.resolve(data);
+          });
         }
     });
 
@@ -108,7 +122,7 @@ app.factory("TasksFactory", function($http, $q, app, User) {
 });
 
 //Clients factory that stores all clients or client pertaining to a job
-app.factory("ClientsFactory", function($http, $q, app, User) {
+app.factory("ClientsFactory", function($http, $q, app, User, local) {
   var o = {
     clients: {},
     client: {}
@@ -119,9 +133,16 @@ app.factory("ClientsFactory", function($http, $q, app, User) {
 
     $.ajax(app.BASE_URL + "/Companies/" + User.user.CompanyID + "/Users/" + User.user.UserID + '/Clients', {
         dataType:'jsonp',
+        timeout: 100,
         success: function(response) { 
           o.clients = response;
           deferred.resolve(response);
+        },
+        error: function(xhr, stat, err) {
+          local.resource("clients").then(function(data){
+            o.clients = data;
+            deferred.resolve(data);
+          });
         }
     });
     return deferred.promise;
@@ -129,10 +150,29 @@ app.factory("ClientsFactory", function($http, $q, app, User) {
   return o;
 });
 
+app.factory("local", function($q){
+  var o = {};
+
+  o.resource = function(route){
+    var deferred = $q.defer();
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      async: false,
+      url: route + ".json"
+    }).done(function(data){
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  }
+  return o;
+});
+
+
 //Jobs factory that stores all jobs and another subset of the jobs pertaining to a corresponding Task
 //For example there is a request for /tasks/1ksj9. '1ksj9' is the :task_id in the router below.
 //We then query for all jobs and then filter the jobs based on whether :task_id is in the PermittedTasks set
-app.factory("JobsFactory", function($http, $q, app, User) {
+app.factory("JobsFactory", function($http, $q, app, User, local) {
   var o = {
     jobs: {},
     job: {}
@@ -143,22 +183,31 @@ app.factory("JobsFactory", function($http, $q, app, User) {
 
     $.ajax(app.BASE_URL + "/Companies/" + User.user.CompanyID + "/Users/" + User.user.UserID + '/Jobs/?withChildIDs=true&callback=JSON_CALLBACK', {
         dataType:'jsonp',
+        timeout: 100,
         success: function(response) { 
           o.jobs = response.map(function(job) {
             job["PermittedTasks"] = job["PermittedTasks"].split(",");
             return job;
           });
           deferred.resolve(response);
+        },
+        error: function(req, stat, err) {
+          local.resource("jobs").then(function(data){
+            o.jobs = data.map(function(job) {
+              job["PermittedTasks"] = job["PermittedTasks"].split(",");
+              return job;
+            });
+            deferred.resolve(data);
+          });
         }
     });
-
     return deferred.promise;
   }
   return o;
 })
 
 //User factory that retrieves all the information about the authorized user
-app.factory("User", function($http, $q, $timeout, app) {
+app.factory("User", function($http, $q, $timeout, app, local) {
   var o = {
     user: {}
   }
@@ -167,18 +216,25 @@ app.factory("User", function($http, $q, $timeout, app) {
     var deferred = $q.defer();
 
     //Promise not working with $http.jsonp(...), so resorting to $.ajax
-    $.ajax(app.BASE_URL + '/session', {
+    $.ajax({
+        url: app.BASE_URL + '/session',
         dataType:'jsonp',
+        timeout: 100,
         success: function(response) { 
           o.user = response;
           deferred.resolve(response);
+        },
+        error: function(xhr, status, error) {
+          local.resource("user").then(function(data){
+            o.user = data;
+            deferred.resolve(data);
+          });
         }
     });
     return deferred.promise;
   }
   return o;
 });
-
 
 //Router configuration for this Single Page App
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -221,8 +277,16 @@ app.config(function($stateProvider, $urlRouterProvider) {
                 return JobsFactory.getJobs().then(function(data) {
 
                   //For all the jobs, check if the task_id is in the set of PermittedTasks 
-                  JobsFactory.job = _.find(data, function(job) {
+                  JobsFactory.job = _.filter(data, function(job) {
                     return _.contains(job.PermittedTasks, $stateParams.task_id);
+                  });
+                  var jobs = _.pluck(JobsFactory.job, "ClientID");
+                  return ClientsFactory.getClients().then(function(clients){
+                    ClientsFactory.client = _.filter(clients, function(client){
+                      return _.contains(jobs, client.ClientID);
+                    });
+                    debugger;
+                    return data;
                   });
                   return ClientsFactory.getClients();
               });
